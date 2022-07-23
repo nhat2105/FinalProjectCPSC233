27c00b69113abb87f6dab993cc6751b0fea6bef3
hong.truong1 <hong.truong1@IC-CS233-30.uc.ucalgary.ca>
2022-07-24 06:19:28 +0700

    Switch default scene to main application view to be worked with

--- src/application/ApplicationController.java
+++ src/application/ApplicationController.java
@@ -8,7 +8,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -20,11 +19,7 @@ public class ApplicationController {
 	double userBMI;
 	
 	@FXML
-	private Label BMILabel;
-	
-	@FXML
 	private Button mealsButton;
-	
 	
 	@FXML
 	private Button sleepTrackerButton;
@@ -41,21 +36,32 @@ public class ApplicationController {
     @FXML
     private TextField userHeightInput;
     
+    /**
     @FXML
     public void getStart(ActionEvent e) throws FileNotFoundException, IOException {
     	calculateUserBMI();
     	System.out.println(String.format("Your BMI is: %.1f", userBMI));
     	
-    	switchToMainScene();
+    	//Switch to main scene
+    	switchScene("src/application/ApplicationView.fxml");
     	
     }
-    private void switchToMainScene() throws FileNotFoundException, IOException {
-    	//Switch to health tracker screen
+    */
+	@FXML
+	public void openSleepTracker(ActionEvent e) throws FileNotFoundException, IOException {
+		System.out.println("Worked");
+		switchScene("src/application/SleepTrackerView.fxml");
+		
+	}
+	
+    private void switchScene(String fileInputStream) throws FileNotFoundException, IOException {
+    	//Switch screen
     	FXMLLoader loader = new FXMLLoader();
-		VBox root = loader.load(new FileInputStream("src/application/ApplicationView.fxml"));
+		VBox root = loader.load(new FileInputStream(fileInputStream));
 		Scene scene = new Scene(root, 600, 400);
     	applicationStage.setScene(scene);
     	applicationStage.show();
+    	
 	}
 
     
