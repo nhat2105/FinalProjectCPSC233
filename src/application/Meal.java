e6813beed810cf42dd44d7b2fc6d482e1b2be536
hong.truong1 <hong.truong1@IC-CS233-8.uc.ucalgary.ca>
2022-08-06 07:58:10 +0700

    Added setter calories for each component of the meal

--- src/application/Meal.java
+++ src/application/Meal.java
@@ -1,5 +1,16 @@
 package application;
 
 public class Meal extends ActivitiesTracker {
+	
+	public void setInfo(String code) {
+		switch(code) { 
+		case "chicken": break; 
+		case "pork": break; 
+		case "rice": break; 
+		case "vegetable": break; 
+		case "soup": break; 
+		case "salmon": break; 
+		}
 
+	}
 }
