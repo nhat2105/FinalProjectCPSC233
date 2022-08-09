fc36b4b932662a4c82389d0accbed0e8ba607e00
hong.truong1 <hong.truong1@IC-CS233-10.uc.ucalgary.ca>
2022-08-09 08:01:47 +0700

    Created new controller and an interface for user's health chart

---
+++ src/application/ChartWindowController.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.TextField;
+public class ChartWindowController {
+	@FXML
+	LineChart<String, Number> sleepLineChart;
+	
+	public void createLineChart(ArrayList<TextField> allSleepInputs) {//take some inputs from other window, then create it
+		XYChart.Series<String, Number> sleepSeries = new XYChart.Series<String, Number>();
+		int dayNumber = 1;
+		for (TextField sleepInput: allSleepInputs) {
+			sleepSeries.getData().add(new XYChart.Data<String, Number>("Day " + dayNumber, Double.parseDouble(sleepInput.getText())));
+			dayNumber++;
+		}
+		
+		sleepLineChart.getData().add(sleepSeries);
+	}
+
+	public void createBarChart() {
+	}
+
+	public void createPieChart() {
+		
+		
+	}
+}
