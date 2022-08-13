86cf3aa6989d789f6d89df4fe15cee822ddc5aa5
hong.truong1 <hong.truong1@IC-CS233-28.uc.ucalgary.ca>
2022-08-14 04:56:32 +0700

    Added comments on each function

--- src/application/Activities.java
+++ src/application/Activities.java
@@ -1,36 +1,46 @@
 package application;
 
 public class Activities{
+	//Instances
 	protected double calories = 0;
 	private String actCode = "";
 	protected String type = "";
 	
+	//Constructor to set activity's code, calories, type in that respectively
 	public Activities(String code, double calories, String type) {
 		setCode(code);
 		setCalories(calories);
 		setType(type);
 	}
-	
-	public double getCaloriesInfo() {
-		return calories;
+	//Copy constructor
+	public Activities(Activities toCopy) {
+		this.type = toCopy.getType();
+		this.calories = toCopy.getCaloriesInfo();
+		this.actCode = toCopy.getCode();
 	}
-	//The function below returns user weight status
-	//based on their BMI
+	
+	//setter and getter for activity's code (name)
 	protected void setCode(String actCode) {
 		this.actCode = actCode;
 		
 	}
-	public void setType(String typeOfAct) {
-		this.type = typeOfAct;
-	}
-	public String getType() {
-		return this.type;
-	}
 	public String getCode() {
 		return actCode;
 	}
+	//setter and getter for activity's type
+	public void setType(String typeOfAct) {
+		this.type = typeOfAct;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	//setter and getter for activity's calories
 	protected void setCalories(double caloriesAmount) {
 		this.calories = caloriesAmount;
+	}
+	public double getCaloriesInfo() {
+		return calories;
 	}
 	
 }
