32b9f59aa53384163c658148c4c35f5147edaab5
nusyba.shifa <nusyba.shifa@IC-CS233-14.uc.ucalgary.ca>
2022-08-13 10:52:17 -0600

    Created copy constructor for 3 classes Meal, Exercises and Activities

--- src/application/Exercises.java
+++ src/application/Exercises.java
@@ -1,10 +1,40 @@
 package application;
 
 public class Exercises extends Activities {
-	public Exercises(String code, double calories, String type) {
-		super(code, calories, type);
+	//Lists for catalog and their data
+	private String[] exerciseList = {"Running", "Jogging", "Swimming", "Push up", "Squat",
+			"Cycling", "Weight lifting", "Pulling up", "Tennis", "Basketball", "Soccer",
+			"Rugby", "Badminton", "Volleyball", "Crunches", "Yoga", "Meditation", "Aerobics"};
+	double getExerciseCalories(String exerciseName ,double userWeight){
+		Double[] exerciseValue = {6.0375*userWeight, 2.625*userWeight, 250.0, 210.0, 240.0,
+				3.78 * userWeight, 2.52 * userWeight, 2 * userWeight, 4.2 * userWeight, 274.0, 300.0,
+				317.0, 114.0, 298.0, 214.0, 100.0, 35.0, 85.0};
+		for (int i = 0; i < exerciseList.length; i++) {
+			if (exerciseName.equalsIgnoreCase(exerciseList[i])) {
+				return exerciseValue[i];
+			}
+		}
+		return 0;
+		
 	}
-
+	
+	//Constructor with parameters to set name, calories consumption
+	//and type of exercise in that order
+	public Exercises(Exercises another) {
+		super(another);
+	}
+	
+	//boolean to get whether an exercise is in exercise catalog
+	boolean inExerciseList(String exerciseName) {
+		for (int i = 0; i < exerciseList.length; i++) {
+			if (exerciseList[i].equalsIgnoreCase(exerciseName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//Setter for calories info
 	public void setCaloriesInfo(double caloriesInfo) {
 		this.calories = caloriesInfo;
 	}
@@ -13,6 +43,7 @@ public class Exercises extends Activities {
 	String getInfo() {
 		return "This activity consumes " + getCaloriesInfo() + " calories in 30 minutes for you";
 	}
+	//String representation of exercise name
 	public String toString() {
 		return "Your activity: " + getCode();
 	}
