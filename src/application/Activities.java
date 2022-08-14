d59f8d8e0eeb5dd575aaaefe7b279bf1579d99df
hong.truong1 <hong.truong1@IC-CS233-6.uc.ucalgary.ca>
2022-08-14 22:03:35 +0700

    Created two new classes with original names in order for merging

---
+++ src/application/Activities.java
@@ -0,0 +1,47 @@
+package application;
+
+public class Activities{
+	
+	//Instances
+	protected double calories = 0;
+	private String actCode = "";
+	protected String type = "";
+	
+	//Constructor to set activity's code, calories, type in that respectively
+	public Activities(String code, double calories, String type) {
+		setCode(code);
+		setCalories(calories);
+		setType(type);
+	}
+	//Copy constructor
+	public Activities(Activity toCopy) {
+		this.type = toCopy.getType();
+		this.calories = toCopy.getCaloriesInfo();
+		this.actCode = toCopy.getCode();
+	}
+	
+	//setter and getter for activity's code (name)
+	protected void setCode(String actCode) {
+		this.actCode = actCode;
+		
+	}
+	public String getCode() {
+		return actCode;
+	}
+	//setter and getter for activity's type
+	public void setType(String typeOfAct) {
+		this.type = typeOfAct;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	//setter and getter for activity's calories
+	protected void setCalories(double caloriesAmount) {
+		this.calories = caloriesAmount;
+	}
+	public double getCaloriesInfo() {
+		return calories;
+	}
+	
+}
